76219ba0a60eb8f5c51212d0a11b5cee0f8239f9
guillermogb <guillermogb@profe208.informatica.cifpaviles.pa>
2025-10-27 10:37:17 +0100

    Pequeños cambios de formato.

--- zoologico/src/zoologico/Aplicacion.java
+++ zoologico/src/zoologico/Aplicacion.java
@@ -1,43 +1,42 @@
 package zoologico;
+
 import zoologico.animales.*;
 import zoologico.trabajadores.*;
 import zoologico.instalaciones.*;
 
 public class Aplicacion {
-	
 
 	public Aplicacion() {
 
-		//Creo ubicaciones
-		Acuario unAcuario=new Acuario("Zona infantil",200);
-		Parque unParque=new Parque("Zona salvaje",5000);
-		Jaula unaJaula=new Jaula("Zona mamíferos",100);
-		
-		//Creo animales
-		Pez unPez=new Bocarte(unAcuario);
-		Delfin unDelfin=new Delfin(unAcuario);
-		Perro unPerro=new Perro("Mestizo","Pepi", unaJaula);
-		
-		//Creo empleados
-		Empleado unEmpleado=new Cuidador("Manolo",24000);
+		// Creo ubicaciones
+		Acuario unAcuario = new Acuario("Zona infantil", 200);
+		Parque unParque = new Parque("Zona salvaje", 5000);
+		Jaula unaJaula = new Jaula("Zona mamíferos", 100);
+
+		// Creo animales
+		Pez unPez = new Bocarte(unAcuario);
+		Delfin unDelfin = new Delfin(unAcuario);
+		Perro unPerro = new Perro("Mestizo", "Pepi", unaJaula);
+
+		// Creo empleados
+		Empleado unEmpleado = new Cuidador("Manolo", 24000);
 		unEmpleado.trabajar(unPerro);
 		unEmpleado.trabajar(unDelfin);
-		unEmpleado=new Limpiador("Luis",20000);
+		unEmpleado = new Limpiador("Luis", 20000);
 		unEmpleado.trabajar(unPerro);
-		
-		//Ejemplos para ilustrar constantes y static.
-		
+
+		// Ejemplos para ilustrar constantes y static.
+
 		System.out.println(unPerro.MAX_NUMERO_PATAS);
 		System.out.println(unDelfin.MAX_NUMERO_PATAS);
 		System.out.println(Mamifero.MAX_NUMERO_PATAS);
-		
-		PezVolador unPezVolador=new PezVolador(unAcuario);
-		//Puedo consultar el MIN_NUMERO_HUEVOS, pero no cambiarlo.
-		System.out.println(Oviparo.MIN_NUMERO_HUEVOS);
 
+		PezVolador unPezVolador = new PezVolador(unAcuario);
+		// Puedo consultar el MIN_NUMERO_HUEVOS, pero no cambiarlo.
+		System.out.println(Oviparo.MIN_NUMERO_HUEVOS);
 	}
-	
+
 	public static void main(String[] args) {
-		Aplicacion a=new Aplicacion();
+		Aplicacion a = new Aplicacion();
 	}
 }
--- zoologico/src/zoologico/animales/Mamifero.java
+++ zoologico/src/zoologico/animales/Mamifero.java
@@ -1,22 +1,23 @@
 package zoologico.animales;
+
 import zoologico.instalaciones.*;
 
 public abstract class Mamifero extends Animal {
 
-	//Los siguientes atributos son finales en Mamifero porque todos 
-	//sus hijos tendrán el mismo valor. No puedo hacerlos finales en
-	//su superclase porque cambia en sus hijos.
+	// Los siguientes atributos son finales en Mamifero porque todos
+	// sus hijos tendrán el mismo valor. No puedo hacerlos finales en
+	// su superclase porque cambia en sus hijos.
 	protected final boolean tienePulmones, tieneBranquias;
-	
-	public static final int MAX_NUMERO_PATAS=4;
-	
+
+	public static final int MAX_NUMERO_PATAS = 4;
+
 	protected Mamifero(Instalacion instalacion) {
 		super(instalacion);
-		this.tieneBranquias=false;
-		this.tienePulmones=true;
+		this.tieneBranquias = false;
+		this.tienePulmones = true;
 		System.out.println("Se ha creado un mamífero.");
 	}
-	
+
 	public void respirar() {
 		System.out.println("Los mamíferos respiran con pulmones");
 	}
